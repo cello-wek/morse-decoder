9d045353cbac337129f251daa8fa618343081dca
cello-wek <falya500@gmail.com>
2021-08-22 22:31:51 +0300

    feat: change js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	if (expr.length % 10 !== 0) {
+		return false;
+	}
+	let arr = [];
+	let decodeSymb;
+	let morseDecode = [];
+	for (let i = 0; i < expr.length; i += 10) {
+		const morseSubstr = expr.substring(i, i + 10);
+		if( morseSubstr === "**********"){
+			morseDecode.push(' ');
+		}
+		for (let j = 0; j < morseSubstr.length; j += 2) {
+			const subSubstring = morseSubstr.substring(j, j + 2);
+			if (subSubstring.includes('00')) {
+				arr.push('');
+			};
+			if (subSubstring.includes('10')) {
+				arr.push('.');
+			}
+			if (subSubstring.includes('11')) {
+				arr.push('-');
+			}
+		}
+			decodeSymb = arr.join('');
+			arr = [];
+		for (let i in MORSE_TABLE) {
+			if (i === decodeSymb) {
+				morseDecode.push(MORSE_TABLE[i]);
+				decodeSymb = '';
+			}
+		}
+	}
+	console.log(morseDecode.join(''));
+	return morseDecode.join('');
 }
 
 module.exports = {
